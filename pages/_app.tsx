f50db24410f4409ab2d3a0119e4fe623fc87636f
Alexander Li <72467504+Alx-Li@users.noreply.github.com>
2022-10-01 09:33:13 -0700

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,32 +3,13 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
 import FirebaseProvider from '../lib/authContext'
 import '../lib/firebaseConfig/init'
-import {AnalyticsProvider} from 'use-analytics'
-import analytics from '../utils/analytics'
-import { useEffect } from 'react'
-import onRouteChange from '@analytics/router-utils'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  useEffect(() => {
-    // run function on when all providers are finished loading
-    //analytics.ready(()=>{
-      analytics.page();
-    //})
-    // run function on route change
-    onRouteChange(()=>{
-      analytics.page()
-    })
-  }, [analytics])
-  
-  return(
-    <AnalyticsProvider instance = { analytics }>
-    <FirebaseProvider>
+  return     <FirebaseProvider>
       <Layout> 
-        <Component {...pageProps} />
-      </Layout>
-    </FirebaseProvider>
-    </AnalyticsProvider>
-  )     
+      <Component {...pageProps} />
+  </Layout>
+  </FirebaseProvider>
 }
 export default MyApp
