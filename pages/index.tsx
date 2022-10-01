6b5479205c92d07c0b9afa78783d663d8c1967ac
Alexander Li <72467504+Alx-Li@users.noreply.github.com>
2022-10-01 13:59:07 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,13 +38,11 @@ const Home: NextPage = () => {
         <title>Signin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="m-auto my-24 w-1/3 h-1/3 divide-y-4 space-y-1"> 
-          <div className="space-y-1">
+      <div className="m-auto my-24 w-1/4 h-1/3 divide-y-4 space-y-1 justify-center"> 
             {/* <input type="email" placeholder="email" className="input input-bordered w-full max-w-xs" onChange={(e) => setEmail(e.target.value)}/>
             <input type="password" placeholder="password" className="input input-bordered w-full max-w-xs" onChange={(e) => setPassword(e.target.value)}/>
             <button className="btn btn-wide justify-center" onClick={login}>Login</button> */}
-            <button className="btn btn-wide justify-center" onClick={loginWithGoogle}>Login with Google</button>
-          </div>
+            <button className="btn btn-wide" onClick={loginWithGoogle}>Login with Google</button>
       </div>
     </>
   )
