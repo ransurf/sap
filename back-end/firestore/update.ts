c7ff0d1f835352243e2047f4a198f7d2d200c0cc
Satvik Garg <47345135+sgarg15@users.noreply.github.com>
2022-10-01 12:00:49 -0700

    Fixed get user data bug

--- back-end/firestore/update.ts
+++ back-end/firestore/update.ts
@@ -2,13 +2,13 @@ import { arrayRemove, updateDoc, doc, arrayUnion } from "firebase/firestore";
 import { db } from "../firebaseConfig/init";
 
 const joinEvent = async (user: any, eventID: string) => {
-  await updateDoc(doc(db, `Users/${user.uid}`), {
+  await updateDoc(doc(db, `Users/${user?.claim.user_id}`), {
     joinedEvents: arrayUnion(eventID),
   });
 };
 
 const leaveEvent = async (user: any, eventID: string) => {
-  await updateDoc(doc(db, `Users/${user.uid}`), {
+  await updateDoc(doc(db, `Users/${user?.claim.user_id}`), {
     joinedEvents: arrayRemove(eventID),
   });
 };
