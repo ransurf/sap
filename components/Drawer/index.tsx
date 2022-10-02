077bee387ae4cbb28c1d35e6adfd1a436b68445f
Alexander Li <72467504+Alx-Li@users.noreply.github.com>
2022-10-02 13:57:11 -0700

    Update index.tsx

--- components/Drawer/index.tsx
+++ components/Drawer/index.tsx
@@ -89,7 +89,7 @@ const Drawer = (props: Props) => {
         </ul>
       </div>
 	  <div>
-	  	<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FVancouver&showNav=0&showTitle=0&showPrint=0&showTabs=0&showCalendars=0&src=c2F0LmdhcmcwM0BnbWFpbC5jb20&color=%23039BE5" width="300" height="600" frameborder="0" scrolling="no"></iframe>
+	  	<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FVancouver&showNav=0&showTitle=0&showPrint=0&showTabs=0&showCalendars=0&src=c2F0LmdhcmcwM0BnbWFpbC5jb20&color=%23039BE5" width="300" height="600"  scrolling="no"></iframe>
 	  </div>
     </div>
   );
